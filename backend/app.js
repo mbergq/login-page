8d21b999912a2f26dec6d601fe39940f57890a96
Martin <106612709+menkodot@users.noreply.github.com>
2024-06-12 10:22:48 +0200

    add protected routes

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 const testRoutes = require("./routes/testRoutes");
+const authRoutes = require("./routes/authRoutes");
+const protectedRoutes = require("./routes/protectedRoutes");
 
 app.use("/", testRoutes);
+app.use("/auth", authRoutes);
+app.use("/protected", protectedRoutes);
 
 const connectionMongoDB = require("./connectionMongoDB");
 connectionMongoDB();
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Port ${port} running..`));
