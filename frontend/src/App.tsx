94966f64497cd895ec845d012af19516204956eb
Martin <106612709+menkodot@users.noreply.github.com>
2024-06-14 11:00:57 +0200

    setup register component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import Home from "./components/Home";
 import Authorize from "./components/Authorize";
+import Register from "./components/Register";
 import { AppLayout } from "./styled-components/AppLayout";
+import { AuthWrapper } from "./styled-components/AuthWrapper";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +10,16 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/auth/register"
+          element={
+            <AppLayout>
+              <AuthWrapper>
+                <Register />
+              </AuthWrapper>
+            </AppLayout>
+          }
+        />
         <Route
           path="/auth"
           element={
--- frontend/src/components/Authorize.tsx
+++ frontend/src/components/Authorize.tsx
@@ -1,17 +1,20 @@
 import { AuthWrapper } from "../styled-components/AuthWrapper.tsx";
 import { Button } from "../styled-components/Button.tsx";
+import { Routes, Route, Link } from "react-router-dom";
 
-function Login() {
+function Authorize() {
   return (
     <AuthWrapper>
       <Button $inputColor="#6855E0" $bgColor={false}>
         Login
       </Button>
-      <Button $inputColor="white" $bgColor={true}>
-        Register
-      </Button>
+      <Link to="/auth/register">
+        <Button $inputColor="white" $bgColor={true}>
+          Register
+        </Button>
+      </Link>
     </AuthWrapper>
   );
 }
 
-export default Login;
+export default Authorize;
