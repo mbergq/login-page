4f92104a6f2c2894c55019ba9351848626624c27
Martin <106612709+menkodot@users.noreply.github.com>
2024-06-27 14:31:36 +0200

    middleware setup on testroute

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const testController = require("../controllers/testController");
+const verifyToken = require("../middleware/authMiddleware");
 
-router.get("/data", testController.getData);
+router.get("/data", verifyToken, testController.getData);
 
 module.exports = router;
