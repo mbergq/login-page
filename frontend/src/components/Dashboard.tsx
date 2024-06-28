9c20376352ec7f45c6c3dd6dcc5be4040f25936a
Martin <106612709+menkodot@users.noreply.github.com>
2024-06-28 10:04:45 +0200

    add fetch to a protected route

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 interface Token {
   webtoken?: string;
 }
@@ -5,6 +8,15 @@ interface Token {
 function Dashboard(props: Token) {
   const { webtoken } = props;
   console.log(webtoken);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/data", {
+        headers: {
+          Authorization: webtoken,
+        },
+      })
+      .then((response) => console.log(response.data));
+  }, [webtoken]);
   return (
     <>
       <h1>Dashboard</h1>
