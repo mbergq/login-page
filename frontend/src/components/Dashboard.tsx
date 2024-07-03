6e7241b055846b975f73822a56e42faaf765fd95
Martin <106612709+menkodot@users.noreply.github.com>
2024-07-03 10:54:23 +0200

    type fetched data

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -5,9 +5,17 @@ interface Token {
   webtoken?: string;
 }
 
+interface Data {
+  _id: string;
+  artist: string;
+  birth: number;
+  records: string[];
+}
+
 function Dashboard(props: Token) {
   const { webtoken } = props;
   const [token, setToken] = useState(webtoken);
+  const [data, setData] = useState<Data[]>([]);
 
   useEffect(() => {
     if (token) {
@@ -21,16 +29,20 @@ function Dashboard(props: Token) {
       setToken(localToken);
     }
     axios
-      .get("http://localhost:3000/data", {
+      .get<Data[]>("http://localhost:3000/data", {
         headers: {
           Authorization: localToken,
         },
       })
-      .then((response) => console.log(response.data));
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      });
   }, []);
   return (
     <>
       <h1>Dashboard</h1>
+      {data && data.map((o) => <p key={o._id}>{o.artist}</p>)}
     </>
   );
 }
