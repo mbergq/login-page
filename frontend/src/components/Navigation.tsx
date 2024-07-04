4489110437eddb405e08f60b45c9a8a62564c364
Martin <106612709+menkodot@users.noreply.github.com>
2024-07-04 14:54:35 +0200

    better layout for navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
+import { NavigationWrapper } from "../styled-components/NavigationWrapper";
 
 function Navigation() {
   return (
-    <div style={{ textAlign: "right" }}>
+    <NavigationWrapper>
+      <Link to="/home">
+        <button>Home</button>
+      </Link>
+      <Link to="/auth" style={{ marginRight: "auto" }}>
+        <button>My dashboard</button>
+      </Link>
       <Link to="/auth/register">
         <button>Register</button>
       </Link>
       <Link to="/auth/login">
         <button>Login</button>
       </Link>
-      <Link to="/auth">
-        <button>My dashboard</button>
-      </Link>
-    </div>
+    </NavigationWrapper>
   );
 }
 
---
+++ frontend/src/styled-components/NavigationWrapper.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const NavigationWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
